8cabd2a9496cdcae3896df159228c39c502998f4
Abhishek Rawat <rawat899@gmail.com>
2022-04-05 12:00:27 +0400

    initial commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "19767a55d4b9b979"; }
+/******/ 		__webpack_require__.h = function() { return "977fbd76447d050f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
