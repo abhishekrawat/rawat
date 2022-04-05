a641e73f6528912c51f9475c3f65b1fa8e966d93
Abhishek Rawat <rawat899@gmail.com>
2022-04-05 15:17:08 +0400

    adding the first basic version

--- pages/index.js
+++ pages/index.js
@@ -6,35 +6,54 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Abhishek Rawat</title>
-        <meta name="description" content="Abhishek Rawat's website" />
+        <title>Abhishek Rawat, Product, UX, UI designer</title>
+        <meta
+          name="description"
+          content="Abhishek Rawat is a product/UX/UI designer with over 12 years of experience specializing in designing healthcare and ecommerce apps"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-      <section class="pb-10">
-
-<div class="py-16 md:py-24 lg:pt-40 lg:pb-52 px-2 md:px-6 w-full">
-  <div class="container px-4 mx-auto relative">
-    <div class="max-w-4xl mx-auto text-center">
-      <span class="text-xl md:text-2xl font-extrabold text-purple-500">Hello!</span>
-      <h1 class="text-5xl sm:text-4xl lg:text-5xl 2xl:text-6xl font-extrabold font-heading mt-1 mb-8">I design delightful Apps</h1>
-      <p class="text-xl md:text-2xl font-extrabold leading-8 mb-8">I specialize in designing healthcare &amp; e-commerce focused apps that are meaningful, easy to use and yet, delightful!</p>
-      <div class="max-w-2xl mx-auto">
-        <div class="flex items-start md:items-center justify-center">
-          <span class="md:text-lg font-extrabold pb-5">Have a project in mind or want to validate your idea, lets get in touch!</span>
-        </div>
-        <div class="flex flex-wrap -mx-2 mb-4 justify-center">
-          <div class="w-full md:w-auto lg:w-2/3 px-2 mb-2 md:mb-0">
-            <input class="inline-block w-full p-4 text-lg font-extrabold placeholder-indigo-900 shadow border-2 border-indigo-900 rounded outline-none" type="email" placeholder="hello@shuffle.dev"/>
+        <section>
+          <div className="w-full px-2 py-16 md:py-24 lg:pt-40 lg:pb-52 md:px-6">
+            <div className="container relative px-4 mx-auto">
+              <div className="max-w-4xl mx-auto text-center">
+                <span className="text-xl font-extrabold text-purple-700 md:text-2xl">
+                  Hello!
+                </span>
+                <h1 className="pb-1 mt-1 mb-8 text-6xl font-bold text-transparent sm:text-7xl lg:text-7xl 2xl:text-7xl font-heading bg-clip-text bg-gradient-to-r from-purple-700 to-blue-600">
+                  I design delightful Apps
+                </h1>
+                <p className="mb-8 text-xl font-bold leading-8 text-white md:text-2xl">
+                  I specialize in designing healthcare &amp; e-commerce focused
+                  apps that are meaningful, easy to use and yet, delightful!
+                </p>
+                <div className="max-w-2xl mx-auto">
+                  <div className="flex items-start justify-center md:items-center">
+                    <span className="pb-5 text-white md:text-lg">
+                      Have a project in mind or want to validate your idea, lets
+                      get in touch!
+                    </span>
+                  </div>
+                  <div className="flex flex-wrap justify-center mb-4 -mx-2">
+                    <div className="w-full px-2 mb-2 md:w-auto lg:w-2/3 md:mb-0">
+                      {/* <input class="inline-block w-full p-4 text-lg font-extrabold placeholder-indigo-900 shadow border-2 border-indigo-900 rounded outline-none" type="email" placeholder="abhishek@rawat.dev"/> */}
+                    </div>
+                    <div className="w-full px-2 md:w-auto lg:w-auto">
+                      <a
+                        href="mailto:abhishek@rawat.dev"
+                        className="inline-flex items-center justify-center w-full h-full px-5 py-4 text-lg font-extrabold leading-6 text-white transition duration-200 rounded md:w-auto hover:bg-indigo-900 border-3 bg-gradient-to-r from-purple-400 to-pink-600"
+                      >
+                        abhishek@rawat.dev
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="w-full md:w-auto lg:w-auto px-2"><a class="inline-flex w-full md:w-auto items-center justify-center h-full py-4 px-5 leading-6 text-lg text-white font-extrabold hover:bg-indigo-900 border-3 rounded transition duration-200 border-indigo-900 bg-purple-500" href="#">Get Started</a></div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-</section>
+        </section>
       </main>
 
       {/* <footer className={styles.footer}>
