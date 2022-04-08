f92e7cb185bee713c5a8a5adba09099d7c6a9469
Abhishek Rawat <rawat899@gmail.com>
2022-04-08 10:57:15 +0400

    fixing bg.png path

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
-import bg from '../public/bg.png'
+import bg from './public/bg.png'
 
 export default function Home() {
   return (
